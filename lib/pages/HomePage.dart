f83bd8084409b245cc880daa065d78b94d4e6dfc
IhorSherstiuk <mycat201198@gmail.com>
2022-03-20 19:14:28 -0700

    Added graph

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:max_watts/model.dart';
 import 'package:max_watts/widgets/appBar.dart';
+import 'package:max_watts/widgets/chart.dart';
 import 'package:max_watts/widgets/entryWidget.dart';
 import 'package:max_watts/widgets/setWidget.dart';
 import 'package:max_watts/widgets/statsWidget.dart';
 import 'package:max_watts/widgets/timer.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,32 +14,37 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      appBar: CustomAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(
-              height: size.height / 3,
-              child: Row(
-                children: const [
-                  Flexible(
-                    flex: 2,
-                    child: SetWidget(),
-                  ),
-                  Flexible(flex: 1, child: Stats()),
-                ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: CustomAppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: size.height / 3,
+                child: Row(
+                  children: const [
+                    Flexible(
+                      flex: 2,
+                      child: SetWidget(),
+                    ),
+                    Flexible(flex: 1, child: Stats()),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Entry(),
-            ),
-            Timer(),
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Entry(),
+              ),
+              Timer(),
+              Expanded(
+                  child:
+                      Chart(Provider.of<WorkoutController>(context).workout!)),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/chart.dart
@@ -0,0 +1,65 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:max_watts/hiveModel.dart';
+import 'package:max_watts/theme.dart';
+
+class Chart extends StatelessWidget {
+  Workout workout;
+  Chart(this.workout, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: purple, width: 1),
+          borderRadius: BorderRadius.circular(20)),
+      child: LineChart(LineChartData(
+        gridData: FlGridData(show: false),
+        titlesData: FlTitlesData(
+            show: false,
+            rightTitles: SideTitles(showTitles: false),
+            topTitles: SideTitles(showTitles: false),
+            bottomTitles: SideTitles(
+                reservedSize: 22,
+                getTextStyles: (context, value) =>
+                    const TextStyle(color: Colors.black, fontSize: 14),
+                showTitles: true,
+                interval: 1,
+                getTitles: (value) => value.toInt().toString(),
+                margin: 12),
+            leftTitles: SideTitles(
+                getTextStyles: (context, value) =>
+                    const TextStyle(color: Colors.black, fontSize: 14),
+                showTitles: true,
+                interval: 1,
+                getTitles: (value) => value.toInt().toString(),
+                margin: 12)),
+        borderData: FlBorderData(show: false),
+        minX: 0,
+        minY: 0,
+        maxX: workout.getSet().length + 3,
+        maxY: workout.max + 20,
+        extraLinesData: ExtraLinesData(horizontalLines: [
+          HorizontalLine(
+            y: workout.plank,
+            strokeWidth: 3,
+            color: Colors.red,
+          )
+        ]),
+        lineBarsData: [
+          LineChartBarData(
+              spots: workout
+                  .getSet()
+                  .asMap()
+                  .entries
+                  .map((e) => FlSpot(e.key.toDouble(), e.value.toDouble()))
+                  .toList(),
+              isCurved: true,
+              barWidth: 5,
+              isStrokeCapRound: true,
+              dotData: FlDotData(show: false)),
+        ],
+      )),
+    );
+  }
+}
